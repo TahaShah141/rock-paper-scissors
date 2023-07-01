1285d48ac21892e3de3d9c0a89af0c1ed039c01e
TahaShah141 <tahashah411@gmail.com>
2023-07-01 05:34:21 +0500

    Change in stucture of choices and make playRound function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+let choices = ["Rock", "Scissors", "Paper"]; //each choice beats the next one
+
 let getComputerChoice = function() {
     let num = Math.round(Math.random() * 100) % 3;
-    switch (num)
-    {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors"
-    }
+    return choices[num];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getIndex(playerSelection);
+    computerSelection = getIndex(computerSelection);
+
+    if (playerSelection == computerSelection) return 0;
+    else if ((playerSelection+1)%3 == computerSelection) return 1; //if computer is the one next of player
+    else return -1; //if not tie or win, must've lost
+}
+
+function getIndex(choiceName) {
+    choiceName = choiceName.toUpperCase();
+    if (choiceName == "ROCK") return 0;
+    else if (choiceName == "SCISSORS") return 1;
+    else if (choiceName == "PAPER") return 2;
+}
+
+let selection = "rocK";
+console.log(playRound(selection, getComputerChoice()));
