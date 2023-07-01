d237dd8870bd31c0d38f817f0db3f7bac2c60942
TahaShah141 <tahashah411@gmail.com>
2023-07-01 05:10:09 +0500

    Make getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+let getComputerChoice = function() {
+    let num = Math.round(Math.random() * 100) % 3;
+    switch (num)
+    {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors"
+    }
+}
+
+console.log(getComputerChoice());
